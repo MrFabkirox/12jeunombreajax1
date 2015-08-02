446f05afa93f44817bbc879c77fbb5b2fc067f3d
fabrice <fabrice.esope@gmail.com>
2015-08-02 19:02:59 +0200

    compare number

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html> 
+<head>
+
+	<title>12jeunombreajax1, jeu</title>
+	<script src="jquery.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+<body>
+<script>
+
+	function showNum(elem) {
+		document.getElementById('messageSpan').innerHTML = elem;
+	}
+
+	function displayNum() {
+		var txt = $('#num').val();
+		$.ajax({
+			url : "jeu.php",
+			data : {num : txt},
+			type : 'GET',
+			success : showNum
+		});
+	}
+	
+
+</script>
+
+
+<div id="right"><?php include("menu.php"); ?></div>
+
+<div id="left">
+
+	<h3>12jeunombreajax1, jeu.html</h3>
+
+	<div id="form">
+
+		<input type="text" id="num" placeholder="tape a num" />
+		<input type="submit" onclick="displayNum()" />
+
+	</div>
+
+	<p><span id="messageSpan">Message Span</span>
+
+</div>
+
+
+</body>
+
+</html>
--- jeu.php
+++ jeu.php
@@ -2,7 +2,24 @@
 
 	session_start();
 
-	$num = $_GET['num'];
-	echo("$num");
+	if(!isset($_SESSION['solution'])) {
+		$_SESSION['solution'] = rand(0, 100);
+		$_SESSION['nbessais'] = 0;
+	}
+
+	$num = intval($_GET['num']);
+	echo($num);
+
+	$_SESSION['nbessais'] ++;
+
+	if($num == $_SESSION['solution']) {
+		echo("bravo !, nombre was ".$_SESSION['solution']);
+	} elseif($num > $_SESSION['solution']) {
+		echo("nb too big");
+		$_SESSION['nbessais'] ++;
+	} elseif($num < $_SESSION['solution']) {
+		echo("nb too small");
+		$_SESSION['nbessais'] ++;
+	}
 
 ?>
